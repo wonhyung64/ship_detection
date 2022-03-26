839fa3dbb69fc6ffd5a1062661140eccacd3dc2f
an-seunghwan <wonhyung64@icloud.com>
2022-03-26 14:16:11 +0900

    fitting

--- predict_thesh.py
+++ predict_thesh.py
@@ -1,6 +1,11 @@
 #%%
 import tensorflow as tf
-
+import ship
 
 #%%
 data_dir = "C:\won\data\optimal_threshold\train.tfrecord"
+dataset = tf.data.TFRecordDataset(f"{data_dir}".encode("unicode_escape")).map(ship.deserialize_feature)
+
+dataset = iter(dataset)
+print(dataset)
+filename, feature_map, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset)
