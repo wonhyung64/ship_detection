903777f57054f20858bc840c9550df7a402d84f8
an-seunghwan <wonhyung64@icloud.com>
2022-04-10 16:52:37 +0900

    feat - extract pooled_roi

--- optimal_nms.py
+++ optimal_nms.py
@@ -1,4 +1,5 @@
 #%% 
+import numpy as np
 import os
 import tensorflow as tf
 import numpy as np
@@ -14,10 +15,15 @@ hyper_params["batch_size"] = batch_size = 1
 img_size = (hyper_params["img_size"], hyper_params["img_size"])
 dataset_name = hyper_params["dataset_name"]
 
-dataset, labels = ship.fetch_dataset_v2(dataset_name, "test", img_size)
+dataset, labels = ship.fetch_dataset_v2(dataset_name, "train", img_size)
 dataset = dataset.map(lambda x, y, z, w: preprocessing_utils.preprocessing_ship(x, y, z, w))
 dataset = dataset.batch(1)
 dataset = iter(dataset)
+
+test_dataset, labels = ship.fetch_dataset_v2(dataset_name, "test", img_size)
+test_dataset = test_dataset.map(lambda x, y, z, w: preprocessing_utils.preprocessing_ship(x, y, z, w))
+test_dataset = test_dataset.batch(1)
+test_dataset = iter(test_dataset)
 
 labels = ["bg"] + labels
 hyper_params["total_labels"] = len(labels)
@@ -46,13 +52,12 @@ total_time = []
 mAP = []
 optimal_threshold = []
 threshold_lst = np.arange(0.5, 1.0, 0.05)
-progress_bar = tqdm(range(4000))
-writer = tf.io.TFRecordWriter(f'C:/won/data/optimal_threshold/test.tfrecord'.encode("utf-8"))
+progress_bar = tqdm(range(10430))
+writer = tf.io.TFRecordWriter(f'C:/won/data/optimal_threshold/train_pooled_roi.tfrecord'.encode("utf-8"))
 
 for _ in progress_bar:
     img, gt_boxes, gt_labels, filename = next(dataset)
     rpn_reg_output, rpn_cls_output, feature_map = rpn_model(img)
-    tf.squeeze(feature_map, axis=0)
     roi_bboxes, _ = postprocessing_utils.RoIBBox(rpn_reg_output, rpn_cls_output, anchors, hyper_params)
     pooled_roi = postprocessing_utils.RoIAlign(roi_bboxes, feature_map, hyper_params)
     dtn_reg_output, dtn_cls_output = dtn_model(pooled_roi)
@@ -69,10 +74,42 @@ for _ in progress_bar:
     optimal_threshold.append(best_threshold)
 
     feature_dic = {
-        "filename":tf.squeeze(filename, axis=0),
-        "feature_map":tf.squeeze(feature_map, axis=0),
-        "dtn_reg_output":tf.squeeze(dtn_reg_output, axis=0),
-        "dtn_cls_output": tf.squeeze(dtn_cls_output, axis=0),
+        "pooled_roi": tf.squeeze(pooled_roi, axis=0),
+        "best_threshold": tf.constant(best_threshold, tf.float32),
+    }
+
+    writer.write(ship.serialize_feature(feature_dic))
+
+print("mAP: %.2f" % (tf.reduce_mean(mAP)))
+
+#%%
+total_time = []
+mAP = []
+optimal_threshold = []
+threshold_lst = np.arange(0.5, 1.0, 0.05)
+progress_bar = tqdm(range(3696))
+writer = tf.io.TFRecordWriter(f'C:/won/data/optimal_threshold/test_pooled_roi.tfrecord'.encode("utf-8"))
+
+for _ in progress_bar:
+    img, gt_boxes, gt_labels, filename = next(test_dataset)
+    rpn_reg_output, rpn_cls_output, feature_map = rpn_model(img)
+    roi_bboxes, _ = postprocessing_utils.RoIBBox(rpn_reg_output, rpn_cls_output, anchors, hyper_params)
+    pooled_roi = postprocessing_utils.RoIAlign(roi_bboxes, feature_map, hyper_params)
+    dtn_reg_output, dtn_cls_output = dtn_model(pooled_roi)
+
+    best_threshold = 0.
+    best_AP = 0.
+    for threshold in threshold_lst:
+        final_bboxes, final_labels, final_scores = postprocessing_utils.Decode(dtn_reg_output, dtn_cls_output, roi_bboxes, hyper_params, iou_threshold=threshold)
+        AP = test_utils.calculate_AP(final_bboxes, final_labels, gt_boxes, gt_labels, hyper_params)
+        if AP >= best_AP: 
+            best_threshold = threshold
+            best_AP = AP
+    mAP.append(best_AP)
+    optimal_threshold.append(best_threshold)
+
+    feature_dic = {
+        "pooled_roi": tf.squeeze(pooled_roi, axis=0),
         "best_threshold": tf.constant(best_threshold, tf.float32),
     }
 
