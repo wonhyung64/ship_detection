11aeb563bef8619ec4762094ea16589b9c25de56
an-seunghwan <wonhyung64@icloud.com>
2022-04-20 22:29:10 +0900

    feat - experimental settings for binary case

--- predict_logistic.py
+++ predict_logistic.py
@@ -10,28 +10,6 @@ from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
 from sklearn.metrics import accuracy_score
 
 #%%
-data_dir = "C:\won\data\optimal_threshold\train.tfrecord"
-dataset = tf.data.TFRecordDataset(f"{data_dir}".encode("unicode_escape")).map(ship.deserialize_feature)
-dataset = dataset.batch(1)
-dataset = iter(dataset)
-
-test_dir = "C:\won\data\optimal_threshold\test.tfrecord"
-dataset_test = tf.data.TFRecordDataset(f"{test_dir}".encode("unicode_escape")).map(ship.deserialize_feature)
-dataset_test = dataset_test.batch(1)
-dataset_test = iter(dataset_test)
-
-#%%
-data_dir = "C:\won\data\optimal_threshold\train_pooled_roi.tfrecord"
-dataset = tf.data.TFRecordDataset(f"{data_dir}".encode("unicode_escape")).map(ship.deserialize_feature_v2)
-dataset = dataset.batch(1)
-dataset = iter(dataset)
-
-test_dir = "C:\won\data\optimal_threshold\test_pooled_roi.tfrecord"
-dataset_test = tf.data.TFRecordDataset(f"{test_dir}".encode("unicode_escape")).map(ship.deserialize_feature_v2)
-dataset_test = dataset_test.batch(1)
-dataset_test = iter(dataset_test)
-
-#%%
 data_dir = "C:\won\data\optimal_threshold\train_binary.tfrecord"
 dataset = tf.data.TFRecordDataset(f"{data_dir}".encode("unicode_escape")).map(ship.deserialize_feature)
 dataset = dataset.batch(1)
@@ -42,140 +20,19 @@ dataset_test = tf.data.TFRecordDataset(f"{test_dir}".encode("unicode_escape")).m
 dataset_test = dataset_test.batch(1)
 dataset_test = iter(dataset_test)
 
-#%% generate data 
-X = []
-y = []
-while True:
-    try:
-        _, feature_map, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset)
-        features = model_utils.VectorizeFeatures()([feature_map, dtn_reg_output, dtn_cls_output])
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        X.append(features)
-        y.append(best_threshold)
-    except: break
-
-
-X = tf.concat(X, axis=0).numpy()
-y = tf.concat(y, axis=0).numpy().squeeze(axis=-1)
-_ = plt.hist(y, bins="auto")
-
-print(X.shape, y.shape)
-print(Counter(y))
-
-#%%
-test_X = []
-test_y = []
-while True:
-    try:
-        _, feature_map, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset_test)
-        features = model_utils.VectorizeFeatures()([feature_map, dtn_reg_output, dtn_cls_output])
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        test_X.append(features)
-        test_y.append(best_threshold)
-    except: break
-
-
-test_X = tf.concat(test_X, axis=0).numpy()
-test_y = tf.concat(test_y, axis=0).numpy().squeeze(axis=-1)
-test_y.shape
-
-_ = plt.hist(test_y, bins="auto")
-
-print(test_X.shape, test_y.shape)
-print(Counter(test_y))
-
-#%%
-X = []
-y = []
-while True:
-    try:
-        _, feature_map, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset)
-        features = tf.reduce_sum(dtn_reg_output, axis=-1)
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        X.append(features)
-        y.append(best_threshold)
-    except: break
-
-
-X = tf.concat(X, axis=0).numpy()
-y = tf.concat(y, axis=0).numpy().squeeze(axis=-1)
-_ = plt.hist(y, bins="auto")
-
-print(X.shape, y.shape)
-print(Counter(y))
-
-#%%
-test_X = []
-test_y = []
-while True:
-    try:
-        _, feature_map, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset_test)
-        features = tf.reduce_sum(dtn_reg_output, axis=-1)
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        test_X.append(features)
-        test_y.append(best_threshold)
-    except: break
-
-
-test_X = tf.concat(test_X, axis=0).numpy()
-test_y = tf.concat(test_y, axis=0).numpy().squeeze(axis=-1)
-test_y.shape
-
-_ = plt.hist(test_y, bins="auto")
-
-print(test_X.shape, test_y.shape)
-print(Counter(test_y))
-
-#%%
-X = []
-y = []
-while True:
-    try:
-        pooled_roi, best_threshold = next(dataset)
-        features = pooled_roi
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        X.append(features)
-        y.append(best_threshold)
-    except: break
-
-
-X = tf.concat(X, axis=0).numpy()
-y = tf.concat(y, axis=0).numpy().squeeze(axis=-1)
-_ = plt.hist(y, bins="auto")
-
-print(X.shape, y.shape)
-print(Counter(y))
-
-#%%
-test_X = []
-test_y = []
-while True:
-    try:
-        pooled_roi, best_threshold = next(dataset_test)
-        features = pooled_roi
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
-        test_X.append(features)
-        test_y.append(best_threshold)
-    except: break
-
-
-test_X = tf.concat(test_X, axis=0).numpy()
-test_y = tf.concat(test_y, axis=0).numpy().squeeze(axis=-1)
-test_y.shape
-
-_ = plt.hist(test_y, bins="auto")
-
-print(test_X.shape, test_y.shape)
-print(Counter(test_y))
-
 #%%
 X = []
 y = []
 while True:
     try:
         feature_map, pooled_roi, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset)
-        features = pooled_roi
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
+        pooled_roi = tf.reduce_max(pooled_roi, axis=[-1, -2])
+
+        features = model_utils.VectorizeFeatures()([feature_map, dtn_reg_output, dtn_cls_output])
+        features = tf.concat([features, pooled_roi], axis=-1)
+
+        best_threshold = tf.cast(tf.round(test_y - 0.1), dtype=tf.int32)
+
         X.append(features)
         y.append(best_threshold)
     except: break
@@ -193,10 +50,14 @@ test_X = []
 test_y = []
 while True:
     try:
-        feature_map, pooled_roi, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset)
-        pooled_roi, best_threshold = next(dataset_test)
-        features = pooled_roi
-        best_threshold = tf.cast(best_threshold * 20 - 10 , dtype=tf.int32)
+        feature_map, pooled_roi, dtn_reg_output, dtn_cls_output, best_threshold = next(dataset_test)
+        pooled_roi = tf.reduce_max(pooled_roi, axis=[-1, -2])
+
+        features = model_utils.VectorizeFeatures()([feature_map, dtn_reg_output, dtn_cls_output])
+        features = tf.concat([features, pooled_roi], axis=-1)
+
+        best_threshold = tf.cast(tf.round(test_y - 0.1), dtype=tf.int32)
+
         test_X.append(features)
         test_y.append(best_threshold)
     except: break
@@ -210,18 +71,15 @@ _ = plt.hist(test_y, bins="auto")
 
 print(test_X.shape, test_y.shape)
 print(Counter(test_y))
-#%%
-X_ = X[:300, :]
-y_ = y[:300]
 
-model = LogisticRegression(multi_class="multinomial", solver="lbfgs")
+#%%
+model = LogisticRegression(multi_class="auto", solver="lbfgs")
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 n_scores = cross_val_score(model, X, y, scoring="accuracy", cv=cv, n_jobs=-1)
 
 print("Mean Accuracy: %.3f (%.3f)" % (mean(n_scores), std(n_scores)))
 
 model.fit(X, y)
-model.fit(X_, y_)
 
 filename = "logistic_model.sav"
 joblib.dump(model, filename)
@@ -230,8 +88,6 @@ joblib.dump(model, filename)
 y_hat = model.predict(X)
 accuracy_score(y, y_hat)
 
-y_hat = model.predict(X_)
-accuracy_score(y_, y_hat)
 
 
 test_y_hat = model.predict(test_X)
