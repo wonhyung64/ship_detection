edf94ce8b40396b0e174a51a5f8566046b30c4ac
Wonhyung Shin <wonhyung64@gmail.com>
2022-07-16 23:52:07 +0900

    feat - pos threshold tunning

--- run_gc.sh
+++ run_gc.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=hgx
 ##
-#SBATCH --job-name=frcnn_voc07
+#SBATCH --job-name=frcnn_gc
 #SBATCH -o SLURM.%N.%j.out
 #SBATCH -e SLURM.%N.%j.err
 ##
@@ -15,6 +15,4 @@ date
 module add CUDA/11.2.2
 module add ANACONDA/2020.11
 
-python /home1/wonhyung64/Github/ship_detection/main.py --data-dir /home1/wonhyung64/data --name gc --anchor-scales 8. 16. 32. --pos-threshold 0.6
-python /home1/wonhyung64/Github/ship_detection/main.py --data-dir /home1/wonhyung64/data --name gc --anchor-scales 8. 16. 32. --pos-threshold 0.55
 python /home1/wonhyung64/Github/ship_detection/main.py --data-dir /home1/wonhyung64/data --name gc --anchor-scales 8. 16. 32. --pos-threshold 0.5
