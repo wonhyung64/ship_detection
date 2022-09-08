1e31ca1a982f36cb8bc4a0306fc0dfd7ec281510
WonHyung Shin <wonhyung64@icloud.com>
2022-09-08 14:57:54 +0900

    hotfix - rnad_flip_horiz axis error

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%%
 from models.faster_rcnn.utils import (
     initialize_process,
     run_process,
@@ -9,9 +10,11 @@ from utils import (
     build_dataset,
 )
 from utils.voucher import build_dataset
+#%%
 
 def main():
     args, run, weights_dir = initialize_process(NEPTUNE_API_KEY, NEPTUNE_PROJECT)
+    args.data_dir = "/Users/wonhyung64/data/aihub_512_512"
 
     # datasets, labels, train_num, valid_num, test_num = load_dataset(
     #     name=args.name, data_dir=args.data_dir, img_size=args.img_size
