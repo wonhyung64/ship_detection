26a9437e5836f233c80d47a30b5c701908376058
WonHyung Shin <wonhyung64@icloud.com>
2022-09-15 19:16:48 +0900

    feat - reconstruction

--- main.py
+++ main.py
@@ -12,28 +12,30 @@ from utils.ship_data_utils import load_dataset
 
 def main():
     args, run, weights_dir = initialize_process(NEPTUNE_API_KEY, NEPTUNE_PROJECT)
-    # args.data_dir = "/Users/wonhyung64/data/aihub_512_512"
+    args.data_dir = "/Users/wonhyung64/data/aihub_512_512"
+    args.name = "aihub"
 
-    datasets, labels, train_num, valid_num, test_num = load_dataset(
-        name=args.name, data_dir=args.data_dir, img_size=args.img_size
-    )
-    # train_set, valid_set, test_set = build_dataset(datasets, args.batch_size)
-    # train_set, valid_set, test_set = build_dataset(args)
-    # train_num , valid_num, test_num = 8000, 100, 1000
-    # labels = ["bg", "ship"]
-
-    # run_process(
-    #     args,
-    #     labels,
-    #     train_num,
-    #     valid_num,
-    #     test_num,
-    #     run,
-    #     train_set,
-    #     valid_set,
-    #     test_set,
-    #     weights_dir,
+    # datasets, labels, train_num, valid_num, test_num = load_dataset(
+    #     name=args.name, data_dir=args.data_dir, img_size=args.img_size
     # )
+
+    # train_set, valid_set, test_set = build_dataset(datasets, args.batch_size)
+    train_set, valid_set, test_set = build_dataset(args)
+    train_num , valid_num, test_num = 8000, 100, 1000
+    labels = ["bg", "ship", "other_floats"]
+
+    run_process(
+        args,
+        labels,
+        train_num,
+        valid_num,
+        test_num,
+        run,
+        train_set,
+        valid_set,
+        test_set,
+        weights_dir,
+    )
 
 
 if __name__ == "__main__":
